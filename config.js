3463485e9fda53a898ab9603e9549b45a6644646
kailin <pooc95@163.com>
2020-10-22 16:28:56 +0800

    添加是否工作日判断

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 // cookie token
 const TOKEN = ``
+const APP_ID = ``
+const APP_SECRET = ``
 
 module.exports.TOKEN = TOKEN
+module.exports.APP_ID = APP_ID
+module.exports.APP_SECRET = APP_SECRET
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const schedule = require('node-schedule');
 const axios = require('axios')
-const config = require('config')
+const config = require('./config')
 
 const TOKEN = config.TOKEN
+const APP_ID = config.APP_ID
+const APP_SECRET = config.APP_SECRET
 const AUTH_TOKEN = `Bearer ${TOKEN}`;
 const TASK_URL = `https://www.erplus.co/taskWeb/filterUserTaskList?pageSize=20&currentPage=0&contactId=0&order=0&taskType=0&taskStatus=1,2,7&taskFeatures=2,4,1,3&startDate=1972-01-01&endDate=2099-12-30&personLabelType=0&personLabelIds=-1&labelType=0&labelIds=-1&prefix=`;
 const START_URL = `https://www.erplus.co/task/v1/statuses/start`;
+const HOLIDAY_URL = `https://www.mxnzp.com/api/holiday/single/${getTime(2)}?app_id=${APP_ID}&app_secret=${APP_SECRET}`
 
 axios.defaults.headers.common["Authorization"] = AUTH_TOKEN;
 
@@ -13,18 +16,36 @@ const  scheduleCronstyle = ()=>{
   // 每天的早上 9 点 0 分 0 秒触发
   schedule.scheduleJob('0 0 9 * * *',()=>{
     start()
-    console.log('定时任务启动:' + getTime());
+    console.log('定时任务启动:' + getTime(1));
   });
 }
 
-function getTime() {
+function getTime(type) {
   const time = new Date()
-  return `${time.getMonth()+1}月${time.getDate()}日 ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+  switch (type) {
+    case 1:
+      return `${time.getMonth()+1}月${time.getDate()}日 ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
+    case 2:
+      return `${time.getFullYear()}${time.getMonth()+1}${time.getDate()}`
+  }
+}
+
+async function isHoliday() {
+  const res = await axios.get(HOLIDAY_URL)
+  if (res.status === 200) {
+    return res.data.data.type !== 0
+  }
+  return true
 }
 
 async function start() {
   console.log(`---------`)
   console.log(`启动任务`)
+  if (await isHoliday()) {
+    console.log(`当前为节假日，暂停打卡`)
+    return
+  }
+
   const res = await axios.get(TASK_URL);
   if (res.status === 200) {
     const itemList = res.data.pageItems;
@@ -32,7 +53,7 @@ async function start() {
       return ele.status === 2;
     });
     if (doingList.length) {
-      console.log(`存在已启动任务：【${doingList[0].topic}】，时间：${getTime()}`);
+      console.log(`存在已启动任务：【${doingList[0].topic}】，时间：${getTime(1)}`);
       return;
     }
 
@@ -47,7 +68,7 @@ async function start() {
       const startRes = await axios.post(START_URL, data);
       if (startRes.status === 200) {
         console.log(
-            `任务：【${filterList[0].topic}】已启动，时间：${getTime()}`
+            `任务：【${filterList[0].topic}】已启动，时间：${getTime(1)}`
         );
       }
     }
