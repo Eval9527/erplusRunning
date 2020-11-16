049ca6304aedf0d55e8cffc2020d49cdb923e86f
kailin <pooc95@163.com>
2020-11-16 09:12:20 +0800

    fix: 修改节假日接口

--- common.js
+++ common.js
@@ -4,7 +4,8 @@ const config = require('./config')
 const APP_ID = config.APP_ID
 const APP_SECRET = config.APP_SECRET
 const HOLIDAY_URL1 = `https://www.mxnzp.com/api/holiday/single/${getTime(2)}?app_id=${APP_ID}&app_secret=${APP_SECRET}`
-const HOLIDAY_URL2 = `https://tool.bitefu.net/jiari/?d=${getTime(2)}`
+// const HOLIDAY_URL2 = `https://tool.bitefu.net/jiari/?d=${getTime(2)}`
+const HOLIDAY_URL2 = `http://timor.tech/api/holiday/info/${getTime(3)}`
 const HOLIDAY_URL = config.HOLIDAY_SWITCH === 1 && APP_ID && APP_SECRET ? HOLIDAY_URL1 : HOLIDAY_URL2
 
 
@@ -17,6 +18,8 @@ function getTime(type) {
       return `${month}月${day}日 ${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
     case 2:
       return `${"" + time.getFullYear() + month + day}`
+    case 3:
+      return `${"" + time.getFullYear() + "-" + month+ "-" + day}`
   }
 }
 
@@ -28,8 +31,11 @@ async function getHolidayType() {
     if (config.HOLIDAY_SWITCH === 1 && res.data.code === 1) {
       return Number(res.data.data.type)
     }
+    // if (config.HOLIDAY_SWITCH === 2 || !APP_ID || !APP_SECRET) {
+    //   return Number(res.data)
+    // }
     if (config.HOLIDAY_SWITCH === 2 || !APP_ID || !APP_SECRET) {
-      return Number(res.data)
+      return Number(res.data.type.type)
     }
   }
   return -1
