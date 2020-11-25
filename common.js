139aa11afc98163264ebdc216b4424ea53d1e71d
kailin <pooc95@163.com>
2020-11-25 09:16:07 +0800

    fix: 修复获取日期类型的问题

--- common.js
+++ common.js
@@ -3,10 +3,15 @@ const config = require('./config')
 
 const APP_ID = config.APP_ID
 const APP_SECRET = config.APP_SECRET
-const HOLIDAY_URL1 = `https://www.mxnzp.com/api/holiday/single/${getTime(2)}?app_id=${APP_ID}&app_secret=${APP_SECRET}`
+
+
+function getHolidayURL() {
+  const HOLIDAY_URL1 = `https://www.mxnzp.com/api/holiday/single/${getTime(2)}?app_id=${APP_ID}&app_secret=${APP_SECRET}`
 // const HOLIDAY_URL2 = `https://tool.bitefu.net/jiari/?d=${getTime(2)}`
-const HOLIDAY_URL2 = `http://timor.tech/api/holiday/info/${getTime(3)}`
-const HOLIDAY_URL = config.HOLIDAY_SWITCH === 1 && APP_ID && APP_SECRET ? HOLIDAY_URL1 : HOLIDAY_URL2
+  const HOLIDAY_URL2 = `http://timor.tech/api/holiday/info/${getTime(3)}`
+
+  return config.HOLIDAY_SWITCH === 1 && APP_ID && APP_SECRET ? HOLIDAY_URL1 : HOLIDAY_URL2
+}
 
 
 function getTime(type) {
@@ -25,8 +30,8 @@ function getTime(type) {
 
 async function getHolidayType() {
   // 工作日（0）、休息日（1）和节假日（2）
-  console.log('请求 HOLIDAY_URL 地址为：' + HOLIDAY_URL)
-  const res = await axios.get(HOLIDAY_URL)
+  console.log('请求 HOLIDAY_URL 地址为：' + getHolidayURL())
+  const res = await axios.get(getHolidayURL())
   if (res.status === 200) {
     if (config.HOLIDAY_SWITCH === 1 && res.data.code === 1) {
       return Number(res.data.data.type)
